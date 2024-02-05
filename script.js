176499d68deaf3cd867e6360f7ecfae975e90e5a
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-05 23:28:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+let textarea = document.getElementsByTagName('textarea')[0];
 function up() {
-  //document.getElementsByTagName('iframe')[0].contentDocument.documentElement.innerHTML = "";
-  document.getElementsByTagName('iframe')[0].contentDocument.documentElement.innerHTML = document.getElementsByTagName('textarea')[0].value;
+  document.getElementsByTagName('iframe')[0].contentDocument.documentElement.innerHTML = textarea.value;
 }
-document.getElementsByTagName('textarea')[0].onkeydown = up
-document.getElementsByTagName('textarea')[0].onkeyup = up
+textarea.onkeydown = up
+textarea.onkeyup = up
+
+/*window.addEventListener("keydown",function (e) {
+  if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) { 
+    e.preventDefault();
+    document.getElementById('search').showModal();
+  }
+})*/
