4e9bc8dfcb39d988fe62c3e2f465f57415e6232f
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-06 16:16:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let textarea = document.getElementsByTagName('textarea')[0];
+let iframe = document.getElementsByTagName('iframe')[0];
 function up() {
-  document.getElementsByTagName('iframe')[0].contentDocument.documentElement.innerHTML = textarea.value;
+  iframe.contentDocument.documentElement.innerHTML = '';
+  iframe.contentDocument.documentElement.insertAdjacentHTML('afterbegin', textarea.value)
 }
 textarea.onkeydown = up
 textarea.onkeyup = up
 
 /*window.addEventListener("keydown",function (e) {
-  if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) { 
+  if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) {
     e.preventDefault();
     document.getElementById('search').showModal();
   }
