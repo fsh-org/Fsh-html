ec3cca1d5fb3fbd4b5afba37fdc8695fedf43f5d
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-06 20:53:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,4 @@ function up() {
 textarea.onkeydown = up
 textarea.onkeyup = up
 
-/*window.addEventListener("keydown",function (e) {
-  if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) {
-    e.preventDefault();
-    document.getElementById('search').showModal();
-  }
-})*/
+keystrokes.bindKey('ctrl + shift + f', () => document.getElementById('search').showModal())
