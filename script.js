ef6c1b086308ec1a5602f5bcd32fcba7d1a95a3e
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-07 18:14:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let html = document.getElementById('html');
 let css = document.getElementById('css');
 let js = document.getElementById('js');
-let iframe = document.getElementById('render');
 
 function up() {
   document.querySelector('div[slot="end"]').innerHTML = '<iframe id="render" title="Rendered content" style="background-color: #fff"></iframe>';
-  
+
+  let iframe = document.getElementById('render');
   iframe.contentDocument.write('<style>'+css.value+'</style>')
   iframe.contentDocument.write('<script>'+js.value+'</script>')
   iframe.contentDocument.write(html.value)
@@ -21,8 +21,8 @@ js.onkeyup = up
 document.onkeydown = search;
 function search(evt) {
   if (!evt) evt = event;
-  console.log(evt)
-  if (evt.ctrlKey && evt.shiftKey && evt.key=='f') {
+  //console.log(evt)
+  if (evt.ctrlKey && evt.key==='F') {
     alert("ss"); 
   }
 }
