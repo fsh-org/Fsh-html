80d1029cbfd0cbb1e0cd1e3ce7830a4e8339376a
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-07 21:21:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
-let html = document.getElementById('html');
-let css = document.getElementById('css');
-let js = document.getElementById('js');
-
 function up() {
   document.querySelector('div[slot="end"]').innerHTML = '<iframe id="render" title="Rendered content" style="background-color: #fff"></iframe>';
 
   let iframe = document.getElementById('render');
-  iframe.contentDocument.write('<style>'+css.value+'</style>')
-  iframe.contentDocument.write('<script>'+js.value+'</script>')
-  iframe.contentDocument.write(html.value)
+  let data = window.monaco.editor.getEditors();
+  iframe.contentDocument.write('<style>'+data[1].getValue()+'</style>')
+  iframe.contentDocument.write('<script>'+data[2].getValue()+'</script>')
+  iframe.contentDocument.write(data[0].getValue())
 }
 
-html.onkeydown = up
-html.onkeyup = up
-css.onkeydown = up
-css.onkeyup = up
-js.onkeydown = up
-js.onkeyup = up
-
-document.onkeydown = search;
-function search(evt) {
-  if (!evt) evt = event;
-  if (evt.ctrlKey && evt.key==='F') {
-    document.getElementById('search').showModal();
-  }
-}
-function codeReplace() {
-  let places = [html,css,js];
-  places.forEach(pl => {
-    pl.value = pl.value.replaceAll(document.getElementById('find').value, document.getElementById('with').value)
-  })
-}
+document.getElementById('html').onkeyup = up;
+document.getElementById('css').onkeyup = up;
+document.getElementById('js').onkeyup = up;
