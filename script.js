0e00a3b5509593b07c6241091698612b34e921b3
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-12 17:07:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let sval = [];
 
 function up() {
-  document.querySelector('div[slot="end"]').innerHTML = '<iframe id="render" title="Rendered content" style="background-color: #fff"></iframe>';
-
-  let iframe = document.getElementById('render');
   let data = window.monaco.editor.getEditors();
   
   let val = [data[0].getValue(), data[1].getValue(), data[2].getValue()]
   if (sval.join('|SEPARATOR|FSH|') == val.join('|SEPARATOR|FSH|')) return;
   sval = val
+
+  document.querySelector('div[slot="end"]').innerHTML = '<iframe id="render" title="Rendered content" style="background-color: #fff"></iframe>';
+  let iframe = document.getElementById('render');
+  
   iframe.contentDocument.write('<style>'+data[1].getValue()+'</style>')
   iframe.contentDocument.write('<script>'+data[2].getValue()+'</script>')
   iframe.contentDocument.write(data[0].getValue())
