fffe0c866a0eb76086079af9ecfd46486d5c8357
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-12 16:51:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+let sval = [];
+
 function up() {
   document.querySelector('div[slot="end"]').innerHTML = '<iframe id="render" title="Rendered content" style="background-color: #fff"></iframe>';
 
   let iframe = document.getElementById('render');
   let data = window.monaco.editor.getEditors();
+  
+  let val = [data[0].getValue(), data[1].getValue(), data[2].getValue()]
+  if (sval.allEqual(val)) return;
+  sval = val
   iframe.contentDocument.write('<style>'+data[1].getValue()+'</style>')
   iframe.contentDocument.write('<script>'+data[2].getValue()+'</script>')
   iframe.contentDocument.write(data[0].getValue())
